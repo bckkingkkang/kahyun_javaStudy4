7ef7b1082d65d0859c1477c5b495cbd5094b34a8
고가현 <bufar1014@gmail.com>
2024-07-08 13:11:36 +0900

    상수 예제 코드 추가

---
+++ src/example01/field/StaticFinalExample01.java
@@ -0,0 +1,8 @@
+package example01.field;
+
+public class StaticFinalExample01 {
+    public static void main(String[] args) {
+        System.out.println("반지름 : " + Earth.EARTH_RADIUS);
+        System.out.println("표면적 : " + Earth.EARTH_AREA);
+    }
+}
