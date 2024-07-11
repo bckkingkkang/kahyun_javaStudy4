88452ca93fdda108da8214d5a91a2c98bcd3b04f
고가현 <bufar1014@gmail.com>
2024-07-11 13:32:51 +0900

    인터페이스 - 강제 타입 변환 예제 코드 추가

--- src/example01/interfaceexample/Bus.java
+++ src/example01/interfaceexample/Bus.java
@@ -5,4 +5,8 @@ public class Bus implements Vehicle{
     public void run() {
         System.out.println("Bus is running");
     }
+
+    public void checkFare() {
+        System.out.println("승차 요금 체크");
+    }
 }
