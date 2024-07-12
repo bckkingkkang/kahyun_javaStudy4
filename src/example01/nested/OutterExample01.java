e6f0b460a64423de8f34da99f7b0799900d853e7
고가현 <bufar1014@gmail.com>
2024-07-12 10:47:25 +0900

    중첩 클래스 - 중첩 클래스에서 바깥 클래스 참조 얻기 예제 코드 추가

---
+++ src/example01/nested/OutterExample01.java
@@ -0,0 +1,10 @@
+package example01.nested;
+
+public class OutterExample01 {
+    public static void main(String[] args) {
+        Outter2 outter = new Outter2();
+        Outter2.Nested nested = outter.new Nested();
+
+        nested.print();
+    }
+}
